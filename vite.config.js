826d13357df1a7a60a9dbdeaf3038a6cd25b43b3
rex <jimingotjams557@gmail.comm>
2024-07-24 17:59:38 +0530

    Correct the name of repo

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,5 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: '/<NewGitVite>/', // Replace with your repo name
+  base: '/NewGitVite/', // Replace with your repo name
 });
